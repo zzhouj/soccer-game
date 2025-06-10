565bd71d04fd51ae21cb2f34fcbd6083ea6d95e8
Justin <457299@qq.com>
2025-06-10 19:26:52 +0800

    player moving flip

--- scenes/player.gd
+++ scenes/player.gd
@@ -7,7 +7,10 @@ enum ControlScheme {CPU, P1, P2}
 @export var speed: float
 
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var player_sprite: Sprite2D = %PlayerSprite
 
+
+var heading := Vector2.RIGHT
 
 func _process(_delta: float) -> void:
 	if control_scheme ==ControlScheme.CPU:
@@ -15,16 +18,28 @@ func _process(_delta: float) -> void:
 	else:
 		handle_player_movement()
 	set_movement_animation()
+	set_heading()
+	flip_sprites()
 	move_and_slide()
-
 
 func handle_player_movement() -> void:
 	var direction = KeyUtils.get_input_vector(control_scheme)
 	velocity = direction * speed
-
 
 func set_movement_animation() -> void:
 	if velocity.length() > 0:
 		animation_player.play("run")
 	else:
 		animation_player.play("idle")
+
+func set_heading() -> void:
+	if velocity.x > 0:
+		heading = Vector2.RIGHT
+	elif velocity.x < 0:
+		heading = Vector2.LEFT
+
+func flip_sprites() -> void:
+	if heading == Vector2.RIGHT:
+		player_sprite.flip_h = false
+	elif heading == Vector2.LEFT:
+		player_sprite.flip_h = true
