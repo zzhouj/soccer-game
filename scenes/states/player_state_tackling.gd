15ed4b5379d09a65d2b1d99343c9a45aa661a467
Justin <457299@qq.com>
2025-06-11 18:00:17 +0800

    added friction when tackling

--- scenes/states/player_state_tackling.gd
+++ scenes/states/player_state_tackling.gd
@@ -1,14 +1,20 @@
 class_name PlayerStateTackling
 extends PlayerState
 
-const DURATION_TACKLE := 200
+const DURATION_PRIOR_RECOVERY := 200
+const GROUND_FRICION := 250.0
 
-var time_start_tackle := Time.get_ticks_msec()
+var is_tackle_complete := false
+var time_finish_tackle := Time.get_ticks_msec()
 
 func _enter_tree() -> void:
 	animation_player.play("tackle")
-	time_start_tackle = Time.get_ticks_msec()
 
-func _process(_delta: float) -> void:
-	if Time.get_ticks_msec() - time_start_tackle > DURATION_TACKLE:
+func _process(delta: float) -> void:
+	if not is_tackle_complete:
+		player.velocity = player.velocity.move_toward(Vector2.ZERO, GROUND_FRICION * delta)
+		if player.velocity == Vector2.ZERO:
+			is_tackle_complete = true
+			time_finish_tackle = Time.get_ticks_msec()
+	elif Time.get_ticks_msec() - time_finish_tackle > DURATION_PRIOR_RECOVERY:
 		state_transition_requested.emit(Player.State.RECOVERING)
